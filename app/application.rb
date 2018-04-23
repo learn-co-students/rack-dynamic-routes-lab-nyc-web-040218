f6dcf6abde605b873187223440e94f5439ba2707
Ryan Hu <ryanhuuu@gmail.com>
2018-04-23 15:23:02 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,43 @@
+require 'pry'
+class Application
+  @@item = []
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      subbed = req.path_info.gsub("/items/", "")
+      # binding.pry
+      flag = true
+      @@items.each do |el|
+        if el.name == subbed
+          flag = false
+          resp.write(el.price)
+        end
+      end
+      if flag
+        resp.write("Item not found")
+        resp.status = 400
+      end
+
+
+
+
+
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+
+
+
+    resp.finish
+  end
+
+
+
+
+
+end
